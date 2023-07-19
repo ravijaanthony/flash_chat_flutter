caa5fa5b707c98c3aaf5252df266d23b8a5d9ca4
Anthony <ravija.20200624@iit.ac.lk>
2023-07-19 12:08:01 +0530

    Add existing project

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flash_chat_flutter/screens/chat_screen.dart';
+import 'package:flash_chat_flutter/screens/registration_screen.dart';
+import 'package:flash_chat_flutter/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flash_chat_flutter/screens/login_screen.dart';
+
+// import 'package:flash_chat/screens/welcome_screen.dart';
+// import 'package:flash_chat/screens/login_screen.dart';
+// import 'package:flash_chat/screens/registration_screen.dart';
+// import 'package:flash_chat/screens/chat_screen.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          bodyMedium: TextStyle(color: Colors.black54),
+        ),
+      ),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+      },
+    );
+  }
+}
+
+// return SizedBox(
+// width: 250.0,
+// child: DefaultTextStyle(
+// style: const TextStyle(
+// fontSize: 30.0,
+// fontFamily: 'Agne',
+// ),
+// child: AnimatedTextKit(
+// animatedTexts: [
+// TypewriterAnimatedText('Discipline is the best tool'),
+// TypewriterAnimatedText('Design first, then code'),
+// TypewriterAnimatedText('Do not patch bugs out, rewrite them'),
+// TypewriterAnimatedText('Do not test bugs out, design them out'),
+// ],
+// onTap: () {
+// print("Tap Event");
+// },
+// ),
+// ),
+// );
---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,118 @@
+import 'package:flash_chat_flutter/screens/login_screen.dart';
+import 'package:flash_chat_flutter/screens/registration_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  static String id = "welcome_screen"; //modifying the variable to make the variable available in the class
+
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+// Enable the ability to ticker provider
+class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation animation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    controller = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+      upperBound: 1,
+    );
+
+    // animation = CurvedAnimation(parent: controller, curve: Curves.decelerate);
+
+    controller.forward();
+
+    controller.addListener(() {
+      setState(() {});
+      print(controller.value);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    height: 60,
+                  ),
+                ),
+                SizedBox(
+                  width: 250,
+                  child: DefaultTextStyle(
+                    style: TextStyle(fontSize: 45.0, fontWeight: FontWeight.w900, color: Colors.blueAccent),
+                    child: AnimatedTextKit(
+                      animatedTexts: [
+                        TypewriterAnimatedText('Flash Chat'),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: Material(
+                elevation: 5.0,
+                color: Colors.lightBlueAccent,
+                borderRadius: BorderRadius.circular(30.0),
+                child: MaterialButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, LoginScreen.id);
+                    //Go to login screen.
+                  },
+                  minWidth: 200.0,
+                  height: 42.0,
+                  child: Text(
+                    'Log In',
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: Material(
+                color: Colors.blueAccent,
+                borderRadius: BorderRadius.circular(30.0),
+                elevation: 5.0,
+                child: MaterialButton(
+                  onPressed: () {
+                    // RegistrationScreen();
+                    Navigator.pushNamed(context, RegistrationScreen.id);
+                    //Go to registration screen.
+                  },
+                  minWidth: 200.0,
+                  height: 42.0,
+                  child: Text(
+                    'Register',
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
